5fa318b891a0f7197c7826999fb5fda421f68368
Sandeep Walia <sandeepwalia@Sandeeps-MacBook-Pro.local>
2018-01-27 13:22:18 +0000

    Initial Commit

---
+++ FYP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FYP
+//
+//  Created by Sandeep Walia on 27/01/2018.
+//  Copyright © 2018 Sandeep Walia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
