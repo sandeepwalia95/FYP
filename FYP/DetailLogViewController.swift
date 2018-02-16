5c36089a83a94e132ef289e367e6d238a99f2d85
Sandeep <sandeepwalia@Sandeeps-MacBook-Pro.local>
2018-02-16 20:14:19 +0000

    Set moodViewColor and progressColor;

--- FYP/DetailLogViewController.swift
+++ FYP/DetailLogViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import MBCircularProgressBar
+import DynamicColor
 
 class DetailLogViewController: UIViewController {
     
     @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var moodView: UIView!
+    
     @IBOutlet weak var sleepProgressView: MBCircularProgressBarView!
     @IBOutlet weak var alcoholProgressView: MBCircularProgressBarView!
     @IBOutlet weak var workProgressView: MBCircularProgressBarView!
@@ -25,9 +28,21 @@ class DetailLogViewController: UIViewController {
         
         self.moodLabel.text = self.log.mood
         
+        let moodColor = moodToColor(mood: self.log.mood)
+        self.moodView.backgroundColor = DynamicColor(hexString: moodColor)
+        
         self.sleepProgressView.value = 0
         self.alcoholProgressView.value = 0
         self.workProgressView.value = 0
+        
+        self.sleepProgressView.progressColor = DynamicColor(hexString: moodColor)
+        self.alcoholProgressView.progressColor = DynamicColor(hexString: moodColor)
+        self.workProgressView.progressColor = DynamicColor(hexString: moodColor)
+        
+        self.sleepProgressView.progressStrokeColor = DynamicColor(hexString: moodColor)
+        self.alcoholProgressView.progressStrokeColor = DynamicColor(hexString: moodColor)
+        self.workProgressView.progressStrokeColor = DynamicColor(hexString: moodColor)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,6 +57,30 @@ class DetailLogViewController: UIViewController {
             self.alcoholProgressView.value = CGFloat(self.log.alcohol)
             self.workProgressView.value = CGFloat(self.log.work)
         }
+    }
+    
+    // Method to convert a string to a mood based on mood retrieved from Firebase within snapshot.
+    func moodToColor(mood: String) -> String {
+        
+        let color: String
+        switch mood {
+        case "Excellent":
+            color = "#976DD0"
+        case "Great":
+            color = "#00A6FF"
+        case "Good":
+            color = "#13CE66"
+        case "Fair":
+            color = "#E9F50C"
+        case "Uh-Oh":
+            color = "#FFBA5C"
+        case "Bad":
+            color = "#F95F62"
+        default:
+            color = "#976DD0"
+        }
+        
+        return color
     }
 
     /*
