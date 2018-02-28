bb2ed8d33980e60de04f323d2f11916086c427c3
Sandeep <sandeepwalia@Sandeeps-MacBook-Pro.local>
2018-02-28 12:34:19 +0000

    setup chart example;

--- FYP/ViewController.swift
+++ FYP/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import SwiftChart
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var chart: Chart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,11 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        let series = ChartSeries([0, 6.5, 2, 8, 4.1, 7, 3.1, 10, 8])
+        chart.xLabels = [0, 3, 6, 9, 12, 15, 18, 21, 24]
+        chart.xLabelsFormatter = { String(Int(round($1))) + "h" }
+        chart.add(series)
     }
 
     override func didReceiveMemoryWarning() {
