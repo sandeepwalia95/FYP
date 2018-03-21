3f968d9703fef436ce8813646a06ea52c13f434d
Sandeep <sandeepwalia@Sandeeps-MacBook-Pro.local>
2018-03-21 10:59:15 +0000

    add functionality to send emails to user (forgot password?)

--- FYP/SignInViewController.swift
+++ FYP/SignInViewController.swift
@@ -73,16 +73,42 @@ class SignInViewController: UIViewController {
         }
     }
     
-    @IBAction func registerPressed(_ sender: Any) {
-        print("Register Pressed")
+    @IBAction func forgotPasswordPressed(_ sender: Any) {
+        print("forgotPasswordPressed")
         
-        let registerViewController = self.storyboard?.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
+        let smtpSession = MCOSMTPSession()
+        smtpSession.hostname = "smtp.gmail.com"
+        smtpSession.username = "sandeepfyp@gmail.com"
+        smtpSession.password = "sandeepfyp11"
+        smtpSession.port = 465
+        smtpSession.authType = MCOAuthType.saslPlain
+        smtpSession.connectionType = MCOConnectionType.TLS
+        smtpSession.connectionLogger = {(connectionID, type, data) in
+            if data != nil {
+                if let string = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
+                    NSLog("Connectionlogger: \(string)")
+                }
+            }
+        }
+        let builder = MCOMessageBuilder()
+        builder.header.to = [MCOAddress(displayName: "Sandeep", mailbox: "sandeepwalia95@hotmail.com")]
+        builder.header.from = MCOAddress(displayName: "FYP", mailbox: "sandeepfyp@gmail.com")
+        builder.header.subject = "Test Email"
+        builder.htmlBody="<p>kackibushek</p>"
         
-        self.present(registerViewController, animated: true)
+        let rfc822Data = builder.data()
+        let sendOperation = smtpSession.sendOperation(with: rfc822Data)
+        sendOperation?.start { (error) -> Void in
+            if (error != nil) {
+                NSLog("Error sending email: \(String(describing: error))")
+            } else {
+                NSLog("Successfully sent email!")
+            }
+        }
     }
     
     func setupViews() {
-        
+        // User will be registered at thios moment so display email address already.
         emailAddressTextField.text = defaults.string(forKey: "emailAddressKey")
         
         signInButton.layer.cornerRadius = 7.0
