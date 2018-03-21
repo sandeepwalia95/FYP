29ed92fb6518c954c7c407c6e0f56959e8d5adc9
Sandeep <sandeepwalia@Sandeeps-MacBook-Pro.local>
2018-03-21 12:26:37 +0000

    setup custom displayAlertmessage for each separate action;

--- FYP/SignInViewController.swift
+++ FYP/SignInViewController.swift
@@ -55,7 +55,7 @@ class SignInViewController: UIViewController {
             // Email and password required to sign in
             if (emailAddressTextField.text?.isEmpty)! ||
                 (passwordTextField.text?.isEmpty)! {
-                displayAlertMessage(alertMessage: "All fields are required")
+                displayAlertMessage(title: "Oops!", alertMessage: "All fields are required")
             }
             
             // Sign in if details are correct
@@ -66,9 +66,9 @@ class SignInViewController: UIViewController {
                 
                 self.present(tabViewController, animated: true)
             } else if (!(emailAddressTextField.text?.contains("@"))!) {
-                displayAlertMessage(alertMessage: "Not a valid email address \n eg. myname@me.com")
+                displayAlertMessage(title: "Not a valid email address", alertMessage: "Use an email address with correct format \n eg. myname@me.com")
             } else {
-                displayAlertMessage(alertMessage: "The email or password is incorrect")
+                displayAlertMessage(title: "Oops!", alertMessage: "The email or password is incorrect")
             }
         }
     }
@@ -86,35 +86,36 @@ class SignInViewController: UIViewController {
         let tempPassword = String((0..<8).map{ _ in passString[Int(arc4random_uniform(UInt32(passString.count)))]})
         print(tempPassword)
         
-//        let smtpSession = MCOSMTPSession()
-//        smtpSession.hostname = "smtp.gmail.com"
-//        smtpSession.username = "sandeepfyp@gmail.com"
-//        smtpSession.password = "sandeepfyp11"
-//        smtpSession.port = 465
-//        smtpSession.authType = MCOAuthType.saslPlain
-//        smtpSession.connectionType = MCOConnectionType.TLS
-//        smtpSession.connectionLogger = {(connectionID, type, data) in
-//            if data != nil {
-//                if let string = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
-//                    NSLog("Connectionlogger: \(string)")
-//                }
-//            }
-//        }
-//        let builder = MCOMessageBuilder()
-//        builder.header.to = [MCOAddress(displayName: "Sandeep", mailbox: userEmailAddress)]
-//        builder.header.from = MCOAddress(displayName: "FYP", mailbox: "sandeepfyp@gmail.com")
-//        builder.header.subject = "Test Email"
-//        builder.htmlBody="<html><body><div><p><h1>Hi \(userFirstName), Forgot your password?</h1></p><p>Temporary password: \(tempPassword)</p><p>We suggest you change your password immediatley from within the app after logging in.</p><p>Thank You.</p></div></body></html>"
-//
-//        let rfc822Data = builder.data()
-//        let sendOperation = smtpSession.sendOperation(with: rfc822Data)
-//        sendOperation?.start { (error) -> Void in
-//            if (error != nil) {
-//                NSLog("Error sending email: \(String(describing: error))")
-//            } else {
-//                NSLog("Successfully sent email!")
-//            }
-//        }
+        let smtpSession = MCOSMTPSession()
+        smtpSession.hostname = "smtp.gmail.com"
+        smtpSession.username = "sandeepfyp@gmail.com"
+        smtpSession.password = "sandeepfyp11"
+        smtpSession.port = 465
+        smtpSession.authType = MCOAuthType.saslPlain
+        smtpSession.connectionType = MCOConnectionType.TLS
+        smtpSession.connectionLogger = {(connectionID, type, data) in
+            if data != nil {
+                if let string = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
+                    NSLog("Connectionlogger: \(string)")
+                }
+            }
+        }
+        let builder = MCOMessageBuilder()
+        builder.header.to = [MCOAddress(displayName: "Sandeep", mailbox: userEmailAddress)]
+        builder.header.from = MCOAddress(displayName: "FYP", mailbox: "sandeepfyp@gmail.com")
+        builder.header.subject = "Test Email"
+        builder.htmlBody="<html><body><div><p><h1>Hi \(userFirstName), Forgot your password?</h1></p><p>Temporary password: \(tempPassword)</p><p>We suggest you change your password immediatley from within the app after logging in.</p><p>Thank You.</p></div></body></html>"
+
+        let rfc822Data = builder.data()
+        let sendOperation = smtpSession.sendOperation(with: rfc822Data)
+        sendOperation?.start { (error) -> Void in
+            if (error != nil) {
+                NSLog("Error sending email: \(String(describing: error))")
+            } else {
+                NSLog("Successfully sent email!")
+                self.displayAlertMessage(title: "Password reset", alertMessage: "An email has been sent to you on how to reset your password.")
+            }
+        }
     }
     
     func setupViews() {
@@ -125,24 +126,13 @@ class SignInViewController: UIViewController {
         signInButton.clipsToBounds = true
     }
     
-    func displayAlertMessage(alertMessage: String) -> Void {
+    func displayAlertMessage(title: String ,alertMessage: String) -> Void {
         DispatchQueue.main.async {
-            let alertController = UIAlertController(title: "Something is wrong!", message: alertMessage, preferredStyle: .alert)
+            let alertController = UIAlertController(title: title, message: alertMessage, preferredStyle: .alert)
             
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default,handler: nil))
             
             self.present(alertController, animated: true, completion: nil)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
